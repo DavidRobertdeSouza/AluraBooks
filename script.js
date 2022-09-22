3b1e27957a6f97901e6ed58bbf4bfa1309edf0a8
David Robert de Souza <david_robert97@outlook.com>
2022-09-22 20:12:37 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 async function buscaEndereco(cep){
+  let mensagemErro = document.querySelector('#erro')
+  let cidade = document.querySelector('#cidade')
+  let logradouro = document.querySelector('#endereco')
+  let estado = document.querySelector('#estado')
+  let bairro = document.querySelector('#bairro')
+
+  cidade.value = ''
+  logradouro.value = ''
+  estado.value = ''
+  bairro.value = ''
+  mensagemErro.innerHTML = ''
   try{
     var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
     var consultaCEPConvertida = await consultaCEP.json()
-    // console.log(consultaCEPConvertida)   
+    if(consultaCEPConvertida.erro){
+      throw Error('Cep não existente!')
+    }
+    
+    cidade.value = consultaCEPConvertida.localidade
+    logradouro.value = consultaCEPConvertida.logradouro
+    estado.value = consultaCEPConvertida.uf
+    bairro.value = consultaCEPConvertida.bairro
+    console.log(consultaCEPConvertida)   
     return consultaCEPConvertida
   } catch(erro){
+    mensagemErro.innerHTML = '<p>CEP inválido. Tente novamente!</p>'
     console.log(erro)
   }
 }
+
+
+let cep = document.querySelector('#cep')
+
+cep.addEventListener('focusout', () => {
+  buscaEndereco(cep.value)
+})
 
 // buscaEndereco('01001000')
 
